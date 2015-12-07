31f38cbc7394717e400d37436787dc9a6b353696
Sebastian Fuentes <sebas6297@gmail.com>
2015-12-07 09:49:05 -0600

    :fire: finished mailing

--- js/sendMail.js
+++ js/sendMail.js
@@ -9,28 +9,29 @@ $(document).ready(function(){
 			 e.preventDefault();
 			 mail = $("#mail").val();
 			 name = $("#name").val();
-			 text = $("#text").val();
-			 subject = $("#subject").val();
-			 if (mail == "" && name == "" && text == "" && subject == "") {
+			 number = $("#number").val();
+			 text = $("#text").val()
+			 if (mail == "" && name == "" && text == "" && text == "") {
 			 	$("#missing").removeClass("hidden");
-			 	$("#missing").addClass("missing"); 
-			} else {
-			 	 $("missing").addClass("hidden");
+			 	$("#missing").addClass("missing");
+			} 
+			else {
+			 	 $("#missing").addClass("hidden");
 				 $.ajax({
 				  type: "POST",
 			      url: "https://mandrillapp.com/api/1.0/messages/send.json",
 				  data:
 				  {
-				      "key": "3pJCe4nkpH3Yhvq_1pWivw",
+				      "key": "wjhmzD_iZFoLUKo7911Gxw",
 				      "message": {
 				          "text": text,
-				          "subject": subject,
+				          "subject": "Mail from the webpage",
 				          "from_email": mail,
 				          "from_name": name,
 				          "to": [
 				              {
 				                  // "email": "charlotteturnbull@gmail.com",
-				                  "email": "sebas6297@gmail.com",
+				                  "email": "sebastian@turninternational.co.uk",
 				                  "name": "Charlotte Turnbull",
 				                  "type": "to"
 				              }
@@ -41,16 +42,17 @@ $(document).ready(function(){
 				      }
 				  }
 				})
-				.done(function(e) {})
-			$("#send").prop('disabled', true);
-			$(".loading").removeClass("hidden");
-			setTimeout(function(){
-				$(".loading").addClass("hidden");
-				$("#send").addClass("disabled");
-				$("#send").val("Ready")
-			}, 3000);
-			Cookies.set('charlotte', "form sent", { expires: 1 });
-			location.reload();
+				.done(function(e, r) {
+					if (e[0].reject_reason == null) {
+		                console.log("sent");
+		                $("#send").prop('disabled', true);
+		                Cookies.set('charlotte', "form sent", { expires: 1 });
+		                console.log("Cookie set");
+    					location.reload();
+		            } else {
+		            	console.log(e[0].reject_reason);
+		            }
+				})
 			}
 		});
 	}
